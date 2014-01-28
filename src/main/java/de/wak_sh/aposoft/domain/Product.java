9ac556b8a4f3d67dc14f3a0dc822c062397a4b1d
Artem Hofmann <artemh92@gmail.com>
2014-01-28 13:12:56 +0100

    Add product domain model

---
+++ src/main/java/de/wak_sh/aposoft/domain/Product.java
@@ -0,0 +1,49 @@
+package de.wak_sh.aposoft.domain;
+
+import javax.persistence.Basic;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import lombok.Data;
+
+@Entity
+@Data
+public class Product {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @Basic
+    private String name;
+
+    @Basic
+    private boolean prescription;
+
+    @Basic
+    private float dosage;
+
+    @Basic
+    private int width;
+
+    @Basic
+    private int height;
+
+    @Basic
+    private int length;
+
+    @ManyToOne
+    @JoinColumn(name = "product_type_id")
+    private ProductType productType;
+
+    @ManyToOne
+    @JoinColumn(name = "product_group_id")
+    private ProductGroup productGroup;
+
+    @ManyToOne
+    @JoinColumn(name = "unit_id")
+    private Unit unit;
+}
