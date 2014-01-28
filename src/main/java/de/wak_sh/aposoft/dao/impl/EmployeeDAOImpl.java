ffac18a2080ca0dd91bcd99d6b875435770d3f4d
Christoph Mende <mende.christoph@gmail.com>
2014-01-28 21:31:34 +0100

    Implement most of EmployeeDAOImpl and UserDAOImpl

--- src/main/java/de/wak_sh/aposoft/dao/impl/EmployeeDAOImpl.java
+++ src/main/java/de/wak_sh/aposoft/dao/impl/EmployeeDAOImpl.java
@@ -2,27 +2,54 @@ package de.wak_sh.aposoft.dao.impl;
 
 import java.util.List;
 
+import javax.persistence.EntityExistsException;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
 import de.wak_sh.aposoft.dao.EmployeeDAO;
 import de.wak_sh.aposoft.domain.Employee;
 
 public class EmployeeDAOImpl implements EmployeeDAO {
 
+    private EntityManagerFactory emf;
+
+    public EmployeeDAOImpl() {
+        emf = Persistence.createEntityManagerFactory("de.wak_sh.aposoft.persistence");
+    }
+
     @Override
     public List<Employee> findAll() {
-        // TODO Auto-generated method stub
-        return null;
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+        Query q = em.createQuery("SELECT e FROM Employee e");
+        em.getTransaction().commit();
+        return q.getResultList();
     }
 
     @Override
     public List<Employee> findByFirstNameAndName(String firstname, String name) {
-        // TODO Auto-generated method stub
-        return null;
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+        Query q = em.createQuery("SELECT e FROM Employee e WHERE e.name = :name AND e.firstName = :firstName");
+        q.setParameter("firstName", firstname);
+        q.setParameter("name", name);
+        em.getTransaction().commit();
+        return q.getResultList();
     }
 
     @Override
     public boolean insertEmployee(Employee employee) {
-        // TODO Auto-generated method stub
-        return false;
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+        try {
+            em.persist(employee);
+        } catch (EntityExistsException e) {
+            return false;
+        }
+        em.getTransaction().commit();
+        return true;
     }
 
     @Override
@@ -33,8 +60,15 @@ public class EmployeeDAOImpl implements EmployeeDAO {
 
     @Override
     public boolean deleteEmployee(Employee employee) {
-        // TODO Auto-generated method stub
-        return false;
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+        try {
+            em.remove(employee);
+        } catch (IllegalArgumentException e) {
+            return false;
+        }
+        em.getTransaction().commit();
+        return true;
     }
 
 }
--- src/main/java/de/wak_sh/aposoft/dao/impl/UserDAOImpl.java
+++ src/main/java/de/wak_sh/aposoft/dao/impl/UserDAOImpl.java
@@ -2,27 +2,53 @@ package de.wak_sh.aposoft.dao.impl;
 
 import java.util.List;
 
+import javax.persistence.EntityExistsException;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
 import de.wak_sh.aposoft.dao.UserDAO;
 import de.wak_sh.aposoft.domain.User;
 
 public class UserDAOImpl implements UserDAO {
 
+    private EntityManagerFactory emf;
+
+    public UserDAOImpl() {
+        emf = Persistence.createEntityManagerFactory("de.wak_sh.aposoft.persistence");
+    }
+
     @Override
     public List<User> findAll() {
-        // TODO Auto-generated method stub
-        return null;
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+        Query q = em.createQuery("SELECT u FROM User u");
+        em.getTransaction().commit();
+        return q.getResultList();
     }
 
     @Override
     public List<User> findUserByUsername(String username) {
-        // TODO Auto-generated method stub
-        return null;
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+        Query q = em.createQuery("SELECT u FROM User u WHERE u.username = :username");
+        q.setParameter("username", username);
+        em.getTransaction().commit();
+        return q.getResultList();
     }
 
     @Override
     public boolean insertUser(User user) {
-        // TODO Auto-generated method stub
-        return false;
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+        try {
+            em.persist(user);
+        } catch (EntityExistsException e) {
+            return false;
+        }
+        em.getTransaction().commit();
+        return true;
     }
 
     @Override
@@ -33,8 +59,15 @@ public class UserDAOImpl implements UserDAO {
 
     @Override
     public boolean deleteuser(User user) {
-        // TODO Auto-generated method stub
-        return false;
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+        try {
+            em.remove(user);
+        } catch (IllegalArgumentException e) {
+            return false;
+        }
+        em.getTransaction().commit();
+        return true;
     }
 
 }
