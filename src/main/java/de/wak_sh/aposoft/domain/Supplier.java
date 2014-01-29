e9e5cf609f1f0bc8216c94efa6076ae2ca9204a6
Artem Hofmann <artemh92@gmail.com>
2014-01-29 12:23:18 +0100

    Fix Supplier domain model

--- src/main/java/de/wak_sh/aposoft/domain/Supplier.java
+++ src/main/java/de/wak_sh/aposoft/domain/Supplier.java
@@ -4,6 +4,8 @@ import javax.persistence.Basic;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 
 import lombok.Data;
 
@@ -30,10 +32,16 @@ public class Supplier {
     @Basic
     private String rating;
 
+    @OneToOne
+    @JoinColumn(name = "address_id")
     private Address address;
 
+    @OneToOne
+    @JoinColumn(name = "payment_condition_id")
     private PaymentCondition paymentCondition;
 
-    private DeliveryCondition deliveryCondion;
+    @OneToOne
+    @JoinColumn(name = "delivery_condition_id")
+    private DeliveryCondition deliveryCondition;
 
 }
