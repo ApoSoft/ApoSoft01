10687ff2345a390dec9a3d432d2907ae8f15f0a8
Artem Hofmann <artemh92@gmail.com>
2014-01-29 12:23:02 +0100

    Fix Insurance domain model

--- src/main/java/de/wak_sh/aposoft/domain/Insurance.java
+++ src/main/java/de/wak_sh/aposoft/domain/Insurance.java
@@ -1,12 +1,18 @@
 package de.wak_sh.aposoft.domain;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 import lombok.Data;
 
 @Entity
 @Data
 public class Insurance {
+
+    @Id
+    @GeneratedValue
+    private int id;
 
     // TODO: lhuebsch - Insurance befuellen sobald Datenmodell fertig
 
