8c9c148f648fe8b38a4df5707d8ca48911d8418b
Artem Hofmann <hofmann.artem@gmail.com>
2014-01-29 12:21:40 +0100

    Update Country domain model

--- src/main/java/de/wak_sh/aposoft/domain/Country.java
+++ src/main/java/de/wak_sh/aposoft/domain/Country.java
@@ -1,9 +1,12 @@
 package de.wak_sh.aposoft.domain;
 
+import java.util.List;
+
 import javax.persistence.Basic;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 import lombok.Data;
 
@@ -20,5 +23,8 @@ public class Country {
 
     @Basic
     private String name;
+
+    @OneToMany(mappedBy = "country")
+    private List<Address> addresses;
 
 }
