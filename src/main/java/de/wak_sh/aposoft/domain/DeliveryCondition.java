a4a77ece2e91104d7338e5978081dcfd0eb2819c
Artem Hofmann <hofmann.artem@gmail.com>
2014-01-29 12:22:45 +0100

    Fix DeliveryCondition domain model

--- src/main/java/de/wak_sh/aposoft/domain/DeliveryCondition.java
+++ src/main/java/de/wak_sh/aposoft/domain/DeliveryCondition.java
@@ -1,12 +1,18 @@
 package de.wak_sh.aposoft.domain;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 import lombok.Data;
 
 @Entity
 @Data
 public class DeliveryCondition {
+
+    @Id
+    @GeneratedValue
+    private int id;
 
     // TODO: lhuebsch - befuelle DeliveryCondition mit Inhalt sobald Datenmodell
     // fertig
