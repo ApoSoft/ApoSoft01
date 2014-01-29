877ebed11396d9bea2b464378ce942c538ff6044
Lennart Huebsch <huebsch.lennart@gmail.com>
2014-01-29 11:16:55 +0100

    Add PaymentCondition entity

---
+++ src/main/java/de/wak_sh/aposoft/domain/PaymentCondition.java
@@ -0,0 +1,34 @@
+package de.wak_sh.aposoft.domain;
+
+import javax.persistence.Basic;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+import org.hibernate.annotations.Type;
+import org.joda.time.DateTime;
+
+@Entity
+@Data
+public class PaymentCondition {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    // Zahlungsziel
+    @Basic
+    @Type(type = "org.jadira.usertype.dateandtime.joda.PersistentLocalDateTime")
+    private DateTime paymentDate;
+
+    // Skontofrist
+    @Basic
+    @Type(type = "org.jadira.usertype.dateandtime.joda.PersistentLocalDateTime")
+    private DateTime discountDate;
+
+    @Basic
+    private float discountValue;
+
+}
