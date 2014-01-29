82b77913e81ad5b6978f4c3bcdccb8b026372bba
Lennart Huebsch <huebsch.lennart@gmail.com>
2014-01-29 11:15:01 +0100

    Add Address entity

---
+++ src/main/java/de/wak_sh/aposoft/domain/Address.java
@@ -0,0 +1,47 @@
+package de.wak_sh.aposoft.domain;
+
+import javax.persistence.Basic;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Entity
+@Data
+public class Address {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    private Country country;
+
+    @Basic
+    private String city;
+
+    @Basic
+    private int postalCode;
+
+    @Basic
+    private String street;
+
+    @Basic
+    private String number;
+
+    @Basic
+    private String extra01;
+
+    @Basic
+    private String extra02;
+
+    @Basic
+    private String extra03;
+
+    @Basic
+    private int phone;
+
+    @Basic
+    private String email;
+
+}
