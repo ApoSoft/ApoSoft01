953f644e21a3656fb8e2115f74ccff1b153bb55f
Artem Hofmann <artemh92@gmail.com>
2014-01-29 11:51:19 +0100

    Add OrderItem domain model

---
+++ src/main/java/de/wak_sh/aposoft/domain/OrderItem.java
@@ -0,0 +1,25 @@
+package de.wak_sh.aposoft.domain;
+
+import javax.persistence.Basic;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import lombok.Data;
+
+@Entity
+@Data
+public class OrderItem {
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @Basic
+    private int count;
+}
