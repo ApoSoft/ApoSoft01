5603ce5e8c39230fb4c769bd3b01d8f3b3faf778
Lennart Huebsch <huebsch.lennart@gmail.com>
2014-01-29 11:16:14 +0100

    Add Insurance and DeliveryCondition entities whithout content

---
+++ src/main/java/de/wak_sh/aposoft/domain/DeliveryCondition.java
@@ -0,0 +1,14 @@
+package de.wak_sh.aposoft.domain;
+
+import javax.persistence.Entity;
+
+import lombok.Data;
+
+@Entity
+@Data
+public class DeliveryCondition {
+
+    // TODO: lhuebsch - befuelle DeliveryCondition mit Inhalt sobald Datenmodell
+    // fertig
+
+}
---
+++ src/main/java/de/wak_sh/aposoft/domain/Insurance.java
@@ -0,0 +1,13 @@
+package de.wak_sh.aposoft.domain;
+
+import javax.persistence.Entity;
+
+import lombok.Data;
+
+@Entity
+@Data
+public class Insurance {
+
+    // TODO: lhuebsch - Insurance befuellen sobald Datenmodell fertig
+
+}
