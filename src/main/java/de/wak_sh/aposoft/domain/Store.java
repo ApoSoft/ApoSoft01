1cc066fe11f239869581cdd14a0a58a0d77dbec3
Jannik Kuptz <jannikkuptz@web.de>
2014-01-31 18:57:58 +0100

    Extend store domain model

--- src/main/java/de/wak_sh/aposoft/domain/Store.java
+++ src/main/java/de/wak_sh/aposoft/domain/Store.java
@@ -1,13 +1,23 @@
 package de.wak_sh.aposoft.domain;
 
+import java.util.List;
+
+import javax.persistence.Basic;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 import lombok.Data;
 
+/**
+ * Entity class for store (Lagerplatz)
+ * 
+ * @author Jannik Kuptz
+ * 
+ */
 @Entity
 @Data
 public class Store {
@@ -15,7 +25,19 @@ public class Store {
     @GeneratedValue
     private int id;
 
+    @Basic
+    private String branch;
+
+    @Basic
+    private int count;
+
+    @Basic
+    private String depot;
+
     @OneToOne
     @JoinColumn(name = "materials_administration_id")
     private MaterialsAdministration materialsAdministration;
+
+    @OneToMany(mappedBy = "store")
+    private List<Product> products;
 }
