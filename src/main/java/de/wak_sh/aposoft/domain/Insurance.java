9c9c4910248e96c6952798c2f0ec91d76f82daa7
Lennart Huebsch <huebsch.lennart@gmail.com>
2014-01-31 12:47:17 +0100

    Add Content to Insurance domain

--- src/main/java/de/wak_sh/aposoft/domain/Insurance.java
+++ src/main/java/de/wak_sh/aposoft/domain/Insurance.java
@@ -1,5 +1,7 @@
 package de.wak_sh.aposoft.domain;
 
+import javax.persistence.Basic;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -19,6 +21,18 @@ public class Insurance {
     @Id
     @GeneratedValue
     private int id;
+
+    @Column(unique = true)
+    private String insuranceIdNumber;
+
+    @Column(unique = true)
+    private String name;
+
+    @Basic
+    private boolean privateInsurance;
+
+    @Basic
+    private String phone;
 
     // TODO: lhuebsch - Insurance befuellen sobald Datenmodell fertig
 
