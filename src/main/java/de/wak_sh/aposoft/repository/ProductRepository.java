9d6f70060fa048aea9afd2c1342169aebe6ac905
Christoph Mende <mende.christoph@gmail.com>
2014-02-07 15:18:35 +0100

    Document ProductRepository

--- src/main/java/de/wak_sh/aposoft/repository/ProductRepository.java
+++ src/main/java/de/wak_sh/aposoft/repository/ProductRepository.java
@@ -8,7 +8,20 @@ import org.springframework.data.repository.query.Param;
 
 import de.wak_sh.aposoft.domain.Product;
 
+/**
+ * JPA repository for Product objects
+ * 
+ * @author Christoph Mende
+ * 
+ */
 public interface ProductRepository extends CrudRepository<Product, Integer> {
+    /**
+     * Search for a product by name, supporting wildcards
+     * 
+     * @param name
+     *            Product name to search for, optionally wildcarded with %
+     * @return List of matching product
+     */
     @Query("SELECT p FROM Product p WHERE p.name LIKE :name")
     List<Product> findByName(@Param("name") String name);
 }
