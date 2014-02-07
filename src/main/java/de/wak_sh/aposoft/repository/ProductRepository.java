bea5973b30be024fc871d2fd89d2f56e142ce6db
Christoph Mende <mende.christoph@gmail.com>
2014-02-07 14:46:55 +0100

    Add custom query to ProductRepository#findByName() to enable wildcard searches

--- src/main/java/de/wak_sh/aposoft/repository/ProductRepository.java
+++ src/main/java/de/wak_sh/aposoft/repository/ProductRepository.java
@@ -2,10 +2,13 @@ package de.wak_sh.aposoft.repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import de.wak_sh.aposoft.domain.Product;
 
 public interface ProductRepository extends CrudRepository<Product, Integer> {
-    List<Product> findByName(String name);
+    @Query("SELECT p FROM Product p WHERE p.name LIKE :name")
+    List<Product> findByName(@Param("name") String name);
 }
