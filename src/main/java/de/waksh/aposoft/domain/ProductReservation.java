31b4568efe2e000b2375a3f30095debf06587748
Artem Hofmann <artemh92@gmail.com>
2014-02-11 10:57:43 +0100

    Add ProductReservation domain model and repository

---
+++ src/main/java/de/waksh/aposoft/domain/ProductReservation.java
@@ -0,0 +1,33 @@
+package de.waksh.aposoft.domain;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+import lombok.Data;
+
+/**
+ * 
+ * @author ahofmann
+ * 
+ */
+
+@Entity
+@Data
+public class ProductReservation {
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+
+    @OneToMany
+    private List<Product> products;
+}
