ef16dcdff77bc28886e24a721569f6d18c2b9fcd
Jenni <Jenni@Jenni-PC>
2014-02-11 14:31:29 +0100

    Create BranchTest

---
+++ src/test/java/de/waksh/aposoft/BranchTest.java
@@ -0,0 +1,47 @@
+package de.waksh.aposoft;
+
+import javax.transaction.Transactional;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import de.waksh.aposoft.domain.Branch;
+import de.waksh.aposoft.repository.BranchRepository;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SpringConfiguration.class)
+@Transactional
+public class BranchTest {
+
+    @Autowired
+    private BranchRepository repository;
+
+    private Branch createBranch() {
+        Branch branch = new Branch();
+
+        branch.setDescription("Beschreibung");
+        branch.setAddress("Straße");
+        branch.setLocation("Hier");
+
+        return branch;
+    }
+
+    @Test
+    public void testFindAll() {
+        Branch branch = createBranch();
+
+        long size = repository.count();
+        long length = 0;
+        repository.save(branch);
+        Iterable<Branch> it = repository.findAll();
+        for (@SuppressWarnings("unused") Branch branch2 : it) {
+            length++;
+        }
+        Assert.assertEquals(size + 1, length);
+    }
+
+}
