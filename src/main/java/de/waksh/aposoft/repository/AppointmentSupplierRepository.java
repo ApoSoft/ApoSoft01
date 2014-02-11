6d3b6146e0ba65919bc78159724154c5268fea67
Sebastian <kecksland@web.de>
2014-02-11 13:05:48 +0100

    Create AppointmentSupplierRepository

---
+++ src/main/java/de/waksh/aposoft/repository/AppointmentSupplierRepository.java
@@ -0,0 +1,8 @@
+package de.waksh.aposoft.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import de.waksh.aposoft.domain.AppointmentSupplier;
+
+public interface AppointmentSupplierRepository extends CrudRepository<AppointmentSupplier, Integer> {
+}
