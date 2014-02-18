1731630956af167412ed2f287667ae57c5912e52
Sebastian <kecksland@web.de>
2014-02-18 13:50:10 +0100

    Update BranchTest

--- src/test/java/de/waksh/aposoft/BranchTest.java
+++ src/test/java/de/waksh/aposoft/BranchTest.java
@@ -1,5 +1,8 @@
 package de.waksh.aposoft;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.transaction.Transactional;
 
 import org.junit.Assert;
@@ -10,6 +13,8 @@ import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import de.waksh.aposoft.domain.Branch;
+import de.waksh.aposoft.domain.Occupation;
+import de.waksh.aposoft.domain.User;
 import de.waksh.aposoft.repository.BranchRepository;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -28,6 +33,24 @@ public class BranchTest {
         branch.setLocation("Hier");
         branch.setManager("Manager");
         branch.setLogo("Logo");
+
+        Occupation occupation = new Occupation();
+        occupation.setQualification("qualifikation");
+        Occupation occu = new Occupation();
+        occu.setQualification("quali");
+        List<Occupation> occupations = new ArrayList<>();
+        occupations.add(occu);
+        occupations.add(occupation);
+        branch.setOccupations(occupations);
+
+        User user = new User();
+        user.setFirstName("first");
+        user.setName("name");
+        User user2 = new User();
+        user2.setFirstName("f");
+        user2.setName("n");
+        List<User> users = new ArrayList<>();
+        branch.setUsers(users);
 
         return branch;
     }
