a62a09fda88f9417b4f651d3ab6a26b40e9d2ff6
Sebastian <kecksland@web.de>
2014-02-18 13:40:29 +0100

    Update AppointmentSupplierTest

--- src/test/java/de/waksh/aposoft/AppointmentSupplierTest.java
+++ src/test/java/de/waksh/aposoft/AppointmentSupplierTest.java
@@ -1,7 +1,11 @@
 package de.waksh.aposoft;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.transaction.Transactional;
 
+import org.joda.time.LocalDate;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -10,6 +14,7 @@ import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import de.waksh.aposoft.domain.AppointmentSupplier;
+import de.waksh.aposoft.domain.OrderItem;
 import de.waksh.aposoft.repository.AppointmentSupplierRepository;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -22,6 +27,17 @@ public class AppointmentSupplierTest {
 
     private AppointmentSupplier createAppointmentSupplier() {
         AppointmentSupplier appointmentSupplier = new AppointmentSupplier();
+        appointmentSupplier.setDate(new LocalDate(2002, 2, 2));
+
+        List<OrderItem> items = new ArrayList<>();
+        OrderItem item = new OrderItem();
+        item.setAmount(20);
+        OrderItem item2 = new OrderItem();
+        item.setAmount(1);
+        items.add(item2);
+        items.add(item);
+        appointmentSupplier.setItems(items);
+
         return appointmentSupplier;
     }
 
