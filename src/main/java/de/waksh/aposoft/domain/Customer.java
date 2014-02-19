1d81a4aea8a8182a83387e7e429a417610c48569
Christoph Mende <mende.christoph@gmail.com>
2014-02-19 19:50:58 +0100

    Add CascadeType to Customer relations

--- src/main/java/de/waksh/aposoft/domain/Customer.java
+++ src/main/java/de/waksh/aposoft/domain/Customer.java
@@ -3,6 +3,7 @@ package de.waksh.aposoft.domain;
 import java.util.List;
 
 import javax.persistence.Basic;
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -47,22 +48,22 @@ public class Customer {
     @Type(type = "org.jadira.usertype.dateandtime.joda.PersistentDateTime")
     private DateTime birthdate;
 
-    @ManyToOne
+    @ManyToOne(cascade = { CascadeType.ALL })
     @JoinColumn(name = "address_id")
     private Address address;
 
-    @ManyToOne
+    @ManyToOne(cascade = { CascadeType.ALL })
     @JoinColumn(name = "insurance_id")
     private Insurance insurance;
 
-    @ManyToOne
+    @ManyToOne(cascade = { CascadeType.ALL })
     @JoinColumn(name = "payment_condition_id")
     private PaymentCondition paymentCondition;
 
-    @OneToMany(mappedBy = "customer")
+    @OneToMany(mappedBy = "customer", cascade = { CascadeType.ALL })
     private List<AppointmentCustomer> appointments;
 
-    @ManyToMany
+    @ManyToMany(cascade = { CascadeType.ALL })
     @JoinTable(joinColumns = { @JoinColumn(name = "customer_id") }, inverseJoinColumns = { @JoinColumn(name = "customer_group_id") })
     private List<CustomerGroup> customerGroups;
 
