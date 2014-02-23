3cec81e5f0aedcc39c455d031ee0d4fc487267d5
Jenni <Jenni@Jenni-PC>
2014-02-23 22:29:46 +0100

    Update RoleTest

--- src/test/java/de/waksh/aposoft/RoleTest.java
+++ src/test/java/de/waksh/aposoft/RoleTest.java
@@ -1,5 +1,8 @@
 package de.waksh.aposoft;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.transaction.Transactional;
 
 import org.junit.Assert;
@@ -9,7 +12,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import de.waksh.aposoft.domain.Permission;
 import de.waksh.aposoft.domain.Role;
+import de.waksh.aposoft.domain.User;
 import de.waksh.aposoft.repository.RoleRepository;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -24,6 +29,26 @@ public class RoleTest {
         Role role = new Role();
 
         role.setDescription("abc");
+
+        User user = new User();
+        user.setFirstName("first");
+        user.setName("name");
+        User user2 = new User();
+        user2.setFirstName("f");
+        user2.setName("n");
+        List<User> users = new ArrayList<>();
+        users.add(user2);
+        users.add(user);
+        role.setUsers(users);
+
+        Permission permission = new Permission();
+        permission.setDescription("test1");
+        Permission permission2 = new Permission();
+        permission2.setDescription("test2");
+        List<Permission> permissions = new ArrayList<>();
+        permissions.add(permission2);
+        permissions.add(permission);
+        role.setPermissions(permissions);
 
         return role;
     }
