ca1b9e98c69ef43a6d37b98a7c451ba42b5d3a86
Jenni <Jenni@Jenni-PC>
2014-02-25 08:59:42 +0100

    Update RecipeTest, StoreTest, UserTest, WorkScheduleTest

--- src/test/java/de/waksh/aposoft/StoreTest.java
+++ src/test/java/de/waksh/aposoft/StoreTest.java
@@ -1,5 +1,8 @@
 package de.waksh.aposoft;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.transaction.Transactional;
 
 import org.junit.Assert;
@@ -9,6 +12,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import de.waksh.aposoft.domain.MaterialsAdministration;
+import de.waksh.aposoft.domain.Product;
 import de.waksh.aposoft.domain.Store;
 import de.waksh.aposoft.repository.StoreRepository;
 
@@ -25,6 +30,20 @@ public class StoreTest {
         store.setBranch("Medikamentenindustrie");
         store.setAmount(10);
         store.setDepot("A");
+
+        MaterialsAdministration materialsAdministration = new MaterialsAdministration();
+        materialsAdministration.setAmount(10);
+        store.setMaterialsAdministration(materialsAdministration);
+
+        List<Product> products = new ArrayList<>();
+        Product product = new Product();
+        product.setName("test");
+        Product product2 = new Product();
+        product2.setName("test2");
+        products.add(product2);
+        products.add(product);
+        store.setProducts(products);
+
         return store;
     }
 
