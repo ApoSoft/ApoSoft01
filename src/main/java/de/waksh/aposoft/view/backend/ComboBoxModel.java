909c8a941ed9816eb8f1dc3663ae4b5ea10cf891
Christoph Mende <mende.christoph@gmail.com>
2014-03-04 10:47:56 +0100

    Document ComboBoxModel

--- src/main/java/de/waksh/aposoft/view/backend/ComboBoxModel.java
+++ src/main/java/de/waksh/aposoft/view/backend/ComboBoxModel.java
@@ -6,33 +6,71 @@ import java.util.List;
 import javax.swing.event.ListDataEvent;
 import javax.swing.event.ListDataListener;
 
+/**
+ * Generic ComboBoxModel
+ * 
+ * @author Artem Hofmann
+ * 
+ * @param <T>
+ *            type of items used in the model
+ */
 public class ComboBoxModel<T> implements javax.swing.ComboBoxModel<T>, ListDataListener {
     private List<ListDataListener> listeners;
     private List<T> items;
     private T selectedItem;
 
+    /**
+     * Construct the ComboBoxModel and initialize listeners and items lists
+     */
     public ComboBoxModel() {
         listeners = new ArrayList<>();
         items = new ArrayList<T>();
     }
 
+    /**
+     * Initialize ComboBoxModel and add items to the model
+     * 
+     * @param items
+     *            list of items to add initially
+     */
     public ComboBoxModel(List<T> items) {
         this();
         addAll(items);
     }
 
+    /**
+     * Add an item to the model
+     * 
+     * @param item
+     *            item to add
+     */
     public void addItem(T item) {
         items.add(item);
     }
 
+    /**
+     * Add a list of items to the model
+     * 
+     * @param items
+     *            list of items to add
+     */
     public void addAll(List<T> items) {
         this.items.addAll(items);
     }
 
+    /**
+     * Remove an item from the model
+     * 
+     * @param item
+     *            item to remove
+     */
     public void removeItem(T item) {
         items.remove(item);
     }
 
+    /**
+     * Remove all items from the model
+     */
     public void clear() {
         items.clear();
     }
